b59f4b04fc80b58a2a924a9e9f21692484eb11df
Mauricio Martinez <mauricio.martinez.chaves@hotmail.com>
2024-08-07 15:34:21 -0500

    refactor(common) changes best practices

--- service/country_service_test.go
+++ service/country_service_test.go
@@ -1,22 +1,27 @@
 package service
 
 import (
+	"errors"
 	"github.com/mauriciomartinezc/real-estate-mc-common/domain"
 	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/mock"
 	"testing"
 )
 
+// Mock del repositorio
 type MockCountryRepository struct {
 	mock.Mock
 }
 
 func (m *MockCountryRepository) GetAll() (*domain.Countries, error) {
 	args := m.Called()
-	return args.Get(0).(*domain.Countries), args.Error(1)
+	if args.Get(0) != nil {
+		return args.Get(0).(*domain.Countries), args.Error(1)
+	}
+	return nil, args.Error(1)
 }
 
-func TestGetCountries(t *testing.T) {
+func TestCountryService_GetCountries_Success(t *testing.T) {
 	mockRepo := new(MockCountryRepository)
 	countryService := NewCountryService(mockRepo)
 
@@ -28,6 +33,21 @@ func TestGetCountries(t *testing.T) {
 
 	assert.NoError(t, err)
 	assert.Equal(t, expectedCountries, countries)
+
+	mockRepo.AssertExpectations(t)
+}
+
+func TestCountryService_GetCountries_Error(t *testing.T) {
+	mockRepo := new(MockCountryRepository)
+	countryService := NewCountryService(mockRepo)
+
+	var nilCountries *domain.Countries
+	mockRepo.On("GetAll").Return(nilCountries, errors.New("database error"))
+
+	countries, err := countryService.Countries()
+
+	assert.Error(t, err)
+	assert.Nil(t, countries)
 
 	mockRepo.AssertExpectations(t)
 }
